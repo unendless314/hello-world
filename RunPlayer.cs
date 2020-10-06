36f5abcd35c5dbd387549ea1bf24e327593f6c50
unendless314 <65386006+unendless314@users.noreply.github.com>
2020-10-06 13:32:26 +0800

    Update RunPlayer.cs

--- RunPlayer.cs
+++ RunPlayer.cs
@@ -10,45 +10,52 @@ public class RunPlayer : MonoBehaviour
 	protected bool AudioStopEventFired = false;
 	protected bool WasPlaying = false;
 	protected bool IsSongPlaying = false;
+	public bool SongFinished = false;
 
-	protected MovShowData Mov;
+	protected MovShowData movData;
+	protected VideoPlayer movPlayer;
 	public float videoTime;
+	public float videoLength;
+	public float showSAT;
 
 	void Start()
 	{
-		Mov = GameObject.Find("Mov").GetComponent<MovShowData>();
-		videoTime = 0;
+		movData = GameObject.Find("Mov").GetComponent<MovShowData>();
+		movPlayer = GameObject.Find("Mov").GetComponent<VideoPlayer>();
+		videoTime = movData.videoTime;
+		videoLength = movData.videoLength;
 	}
 
 	void Update()
 	{
-		/*
+		videoTime = movData.GetVideoTime();
+
 		if (IsPlaying())
 		{
 			AudioStopEventFired = false;
 			WasPlaying = true;
 			UpdateSmoothAudioTime();
 		}
-		*/
 
-		videoTime = Mov.GetVideoTime();
+		showSAT = SmoothAudioTime;
 	}
 
 	protected void OnSongStopped()
 	{
-		if (!GetComponent<AudioSource>().clip)
+		if (!movPlayer.clip)
 		{
+
 			return;
 		}
 
 		//I want to check if the song has finished playing automatically.
 		//Sometimes this is triggered when the song is at the end, 
 		//and sometimes it has already been reset to the beginning of the song.
-		if (GetComponent<AudioSource>().time == GetComponent<AudioSource>().clip.length
-		 || (WasPlaying && GetComponent<AudioSource>().time == 0))
+		if (movPlayer.time == movPlayer.clip.length
+		 || (WasPlaying && movPlayer.time == 0))
 		{
 			IsSongPlaying = false;
-			//GetComponent<GuitarGameplay>().OnSongFinished();	暫時藏起來看效果
+			SongFinished = true;
 		}
 	}
 
@@ -58,9 +65,9 @@ public class RunPlayer : MonoBehaviour
 		//as smoothly. This uses Time.deltaTime to progress the audio time
 		SmoothAudioTime += Time.deltaTime;
 
-		if (SmoothAudioTime >= GetComponent<AudioSource>().clip.length) //播放畫面時間大於音檔長度時，遊戲結束
+		if (SmoothAudioTime >= videoLength) //播放畫面時間大於音檔長度時，遊戲結束
 		{
-			SmoothAudioTime = GetComponent<AudioSource>().clip.length;
+			SmoothAudioTime = videoLength;
 			OnSongStopped();
 		}
 
@@ -81,53 +88,54 @@ public class RunPlayer : MonoBehaviour
 		}
 
 		//Dont check this at the end of the song	//從音檔長度倒數三秒開始就不再檢查，pitch 有調整的話這個參數也要修正才能以正常速度播放遊戲畫面
-		if (SmoothAudioTime > GetComponent<AudioSource>().clip.length - 3f)
+		if (SmoothAudioTime > videoLength - 3f)
 		{
 			return false;
 		}
 
 		//Check if my smooth time and the actual audio time are of by 0.1
-		return Mathf.Abs(SmoothAudioTime - GetComponent<AudioSource>().time) > 0.1f;    //想玩慢動作版的話，畫面修正秒數一定要縮短，不然畫面回溯超可怕
+		return Mathf.Abs(SmoothAudioTime - videoTime) > 0.05f;    //想玩慢動作版的話，畫面修正秒數一定要縮短，不然畫面回溯超可怕，但是會加倍運算量
 	}
 
 	protected void CorrectSmoothAudioTime() //強制讓畫面播放秒數與音檔播放秒數相等
 	{
-		SmoothAudioTime = GetComponent<AudioSource>().time;
+		SmoothAudioTime = videoTime;
 	}
 
 	public void Play()  //不算很懂
 	{
+		SongFinished = false; // 按下播放鈕，表示歌曲還沒播完
 		IsSongPlaying = true;
 
 		if (SmoothAudioTime < 0)    //負的秒數超過 offset 秒數的話，遊戲會直接開始
 		{
-			StartCoroutine(PlayDelayed(Mathf.Abs(SmoothAudioTime)));    //這個秒數直接決定了遊戲畫面要延遲幾秒才下音樂，很接近 2.8 秒
+			//StartCoroutine(PlayDelayed(Mathf.Abs(SmoothAudioTime)));    //這個秒數直接決定了遊戲畫面要延遲幾秒才下音樂，我先拿掉
+			SmoothAudioTime = videoTime;
 		}
 		else
 		{
-			//GetComponent<AudioSource>().Play();
-			//SmoothAudioTime = GetComponent<AudioSource>().time;
+			SmoothAudioTime = videoTime;
 		}
 	}
 
+	/* 原本程式碼有，但目前用不到
 	protected IEnumerator PlayDelayed(float delay)
 	{
 		yield return new WaitForSeconds(delay);
-
-		//GetComponent<AudioSource>().Play();
 	}
+	*/
 
 	public void Pause()
 	{
 		IsSongPlaying = false;
-		GetComponent<AudioSource>().Pause();    //暫停播放，應該是Unity內建
+		SongFinished = false;
 	}
 
 	public void Stop()
 	{
-		GetComponent<AudioSource>().Stop(); //停止播放，應該是Unity內建
 		WasPlaying = false;
 		IsSongPlaying = false;
+		SongFinished = true;
 	}
 
 	public bool IsPlaying()
@@ -138,20 +146,16 @@ public class RunPlayer : MonoBehaviour
 	public void SetSong(SonataSongData song)
 	{
 		Song = song;
-		GetComponent<AudioSource>().time = 0;    //從第幾秒開始播放
-		GetComponent<AudioSource>().clip = Song.BackgroundTrack; //抓歌曲
-		GetComponent<AudioSource>().pitch = 1;   //會改變音高，超過1會加速而且變尖銳，小數值會變慢變低音，畫面會回溯很可怕
-
 		SmoothAudioTime = MyMath.BeatsToSeconds(-Song.AudioStartBeatOffset, Song.BeatsPerMinute);   //開頭播放前的節拍數轉為秒數取負值，功能未知
 	}
 
 	public float GetCurrentBeat(bool songDataEditor = false)    //現在到底是第幾拍：先將播放時間秒數取出轉換為拍數，再扣掉開頭沒聲音的節拍數
 	{
-		if (songDataEditor)
-		{
-			SmoothAudioTime = GetComponent<AudioSource>().time;
-		}
-
 		return MyMath.SecondsToBeats(SmoothAudioTime, Song.BeatsPerMinute) - Song.AudioStartBeatOffset;
+	}
+
+	public float GetSmoothAudioTime()
+    {
+		return SmoothAudioTime;
 	}
 }
